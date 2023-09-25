799efa6839255d34a1705b56535b50414017cdfc
ViperOne <68926315+The-Viper-One@users.noreply.github.com>
2023-09-25 13:29:49 +0100

    Update PsMapExec.ps1

--- PsMapExec.ps1
+++ PsMapExec.ps1
@@ -3157,7 +3157,7 @@ if ($wait) {
 }
 
 $tcpClient.Close()
-if (!$connected) {return}   elseif ($Connected){
+if (!$connected) {continue}   elseif ($Connected){
 
         if ($Method -eq "GenRelayList" -and $Option -ne "Parse") {
             $Signing = Get-SMBSigning -Target $ComputerName
